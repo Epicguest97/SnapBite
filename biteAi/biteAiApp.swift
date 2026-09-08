dfd8fffb5455cea5d6e4a82279e1c8e59a3bb288
Epicguest97 <kaushikmehul94@gmail.com>
2026-09-08 14:31:01 +0530

    Initial Commit

---
+++ biteAi/biteAiApp.swift
@@ -0,0 +1,17 @@
+//
+//  biteAiApp.swift
+//  biteAi
+//
+//  Created by Mehul Kaushik on 08/09/26.
+//
+
+import SwiftUI
+
+@main
+struct biteAiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
